4ca0988c8d197f484d288be6fbcbbbeef86519fb
darshita55 <220701055@rajalakshmi.edu.in>
2024-11-02 20:56:22 +0530

    Update udp_echo_client.py

--- EX 12A- ECHO CLIENT USING TCP,UDP/udp_echo_client.py
+++ EX 12A- ECHO CLIENT USING TCP,UDP/udp_echo_client.py
@@ -5,7 +5,5 @@ def udp_echo_client(host='localhost', port=12345, message='Hello, World!'):
         client_socket.sendto(message.encode(), (host, port))
         data, _ = client_socket.recvfrom(1024)
         print(f"Received from server: {data.decode()}")
-
-# Run the UDP echo client
 if __name__ == "__main__":
     udp_echo_client(message="Hello, darshita!")
