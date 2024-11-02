ae33ca7d3d5f9b6bcb57e86edef0c877cea3f668
darshita55 <220701055@rajalakshmi.edu.in>
2024-11-02 20:54:26 +0530

    Add files via upload

---
+++ EX 12A- ECHO CLIENT USING TCP,UDP/udp_echo_client.py
@@ -0,0 +1,11 @@
+import socket
+
+def udp_echo_client(host='localhost', port=12345, message='Hello, World!'):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
+        client_socket.sendto(message.encode(), (host, port))
+        data, _ = client_socket.recvfrom(1024)
+        print(f"Received from server: {data.decode()}")
+
+# Run the UDP echo client
+if __name__ == "__main__":
+    udp_echo_client(message="Hello, darshita!")
